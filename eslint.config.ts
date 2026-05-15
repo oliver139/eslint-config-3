629b0fb0f658ddd693678a0a406cdf76fabd5b4f
Oliver Mak <oliver.mak@naxos.com>
2026-05-15 16:36:49 +0800

    Take away tailwind from eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,8 +2,4 @@ import antfu from '@antfu/eslint-config'
 
 import { options } from './src'
 
-export default antfu(...options({
-  tailwind: {
-    entryPoint: 'app/assets/css/main.css',
-  },
-}))
+export default antfu(...options())
